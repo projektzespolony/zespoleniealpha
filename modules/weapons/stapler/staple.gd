e5c17151d13164f32c6f32bab5e6bdb3a0d5c8db
Hubert Parzych <184960707+hparzychrec@users.noreply.github.com>
2024-11-20 04:02:55 +0100

    Preliminary project setup

---
+++ modules/weapons/stapler/staple.gd
@@ -0,0 +1,16 @@
+extends Area2D
+
+var speed = 2000
+var decayFactor = 0.95  #zanik predkosci
+
+
+func _physics_process(delta):
+	position += transform.x * speed * delta
+	speed *= decayFactor
+	if speed <= 50:  # dodanie zasięgu
+		queue_free()
+
+
+func _on_area_entered(area: Area2D) -> void:
+	area.queue_free()
+	queue_free()
