aac92d4ebd4cc34527791caf6809ed8bfabdb0ad
MatKarnicki <matkarnicki@gmail.com>
2024-12-01 13:39:01 +0100

    Stats implementation, movement_speed based on stats

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,11 +1,15 @@
 extends CharacterBody2D
 
 @export var speed = 400
+@export var baseStats: startingStats
+@onready var stats = $Stats
 
+func _ready() -> void:
+	stats.initialize(baseStats)
 func get_input():
 	look_at(get_global_mouse_position())
 	var input_direction = Input.get_vector("left", "right", "up", "down")
-	velocity = input_direction * speed
+	velocity = input_direction * stats.speed
 
 func _physics_process(_delta):
 	get_input()
