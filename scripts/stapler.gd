5557083d5e7b1770388a749b755c77d8d1b98abc
MatKarnicki <matkarnicki@gmail.com>
2024-12-01 15:23:49 +0100

    Split attack_speed and attack_dmg for melee and ranged weapons

--- scripts/stapler.gd
+++ scripts/stapler.gd
@@ -14,17 +14,17 @@ func _process(_delta: float) -> void:
 		melee()
 		
 func shoot():
-	var shoot_speed = player.stats.attack_speed * 2 if player else 1.0
+	var shoot_speed = player.stats.ranged_attack_speed if player else 1.0
 	shoot_cooldown = true
 	var bullet = Staple.instantiate()
-	bullet.damage = player.stats.attack_damage
+	bullet.damage = player.stats.ranged_attack_damage
 	get_tree().get_root().add_child(bullet)
 	bullet.transform = $Muzzle.global_transform
 	await get_tree().create_timer(1.0 / shoot_speed).timeout
 	shoot_cooldown = false
 
 func melee():
-	var attack_speed = player.stats.attack_speed if player else 1.0
+	var attack_speed = player.stats.melee_attack_speed if player else 1.0
 	melee_cooldown = true
 	$MeleeArea.get_child(0).set_disabled(false)
 	await get_tree().create_timer(0.2).timeout
@@ -34,6 +34,6 @@ func melee():
 
 
 func _on_melee_area_area_entered(area: Area2D) -> void:
-	print("MELEE: Hit ", area, "for ", player.stats.attack_damage)
+	print("MELEE: Hit ", area, "for ", player.stats.melee_attack_damage)
 	#TODO: change to dealDmg() - requires enemy implementation
 	area.queue_free()
