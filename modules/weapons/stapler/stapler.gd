514344ddc84098f8ac9479b78a7aae99f22b24cf
MatKarnicki <matkarnicki@gmail.com>
2024-12-05 10:04:40 +0100

    Franke BUGFIX, Finalized implementation of stats

--- modules/weapons/stapler/stapler.gd
+++ modules/weapons/stapler/stapler.gd
@@ -1,30 +1,35 @@
 extends Sprite2D
 
-@export var Staple: PackedScene
-@export var player : CharacterBody2D
+@export var staple: PackedScene
+@export var player: CharacterBody2D
 
 var melee_cooldown = false
 var shoot_cooldown = false
 
-func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("shoot"):
+
+func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed("shoot") and not shoot_cooldown:
 		shoot()
 
-	if Input.is_action_just_pressed("melee_attack"):
+	if Input.is_action_just_pressed("melee_attack") and not melee_cooldown:
 		melee()
 
+
 func shoot() -> void:
-	var shoot_speed = player.stats.ranged_attack_speed if player else 1.0
+	var shoot_speed = player.stats.ranged_attack_damage * player.stats.ranged_attack_speed_modifier
 	shoot_cooldown = true
-	var staple_instance = Staple.instantiate()
-	staple_instance.position = $Muzzle.global_position
-	staple_instance.rotation = $Muzzle.global_rotation
+	var staple_instance = staple.instantiate()
+	staple_instance.transform = $Muzzle.global_transform
+	staple_instance.damage = (
+		player.stats.ranged_attack_damage * player.stats.ranged_attack_damage_modifier
+	)
 	get_tree().get_root().add_child(staple_instance)
 	await get_tree().create_timer(1.0 / shoot_speed).timeout
 	shoot_cooldown = false
 
+
 func melee() -> void:
-	var attack_speed = player.stats.melee_attack_speed if player else 1.0
+	var attack_speed = player.stats.melee_attack_speed * player.stats.melee_attack_speed_modifier
 	melee_cooldown = true
 	$MeleeArea.get_child(0).set_disabled(false)  # CollisionShape2D
 	await get_tree().create_timer(0.2).timeout
@@ -32,6 +37,10 @@ func melee() -> void:
 	await get_tree().create_timer(1.0 / attack_speed).timeout
 	melee_cooldown = false
 
-func _on_area_entered(area: Area2D) -> void:
-	if area.has_method("take_damage"):
-		area.take_damage(10)
+
+func _on_melee_area_body_entered(body: Node2D) -> void:
+	if body.has_method("take_damage"):
+		var damage_to_deal = (
+			player.stats.melee_attack_damage * player.stats.melee_attack_damage_modifier
+		)
+		body.take_damage(damage_to_deal)
