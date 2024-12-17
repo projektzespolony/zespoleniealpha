417e2223d382c6147120cc4b0febce3d56ef85b0
MatKarnicki <matkarnicki@gmail.com>
2024-12-17 22:42:42 +0100

    all nighter, energy drink

---
+++ modules/items/all_nighter/all_nighter.gd
@@ -0,0 +1,7 @@
+class_name AllNighter extends BaseItem
+
+
+func _on_interact() -> void:
+	change_player_damage_reduction(item_stat_transformations.damage_reduction)
+	change_player_ranged_attack_damage(item_stat_transformations.ranged_attack_damage)
+	change_player_melee_attack_damage(item_stat_transformations.melee_attack_damage)
