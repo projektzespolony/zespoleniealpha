d6f206ce4b6d55b85c97f7a8fb7d257d0728554f
MatKarnicki <matkarnicki@gmail.com>
2025-01-14 14:51:14 +0100

    bug fixes

--- modules/interaction/spawner/spawner.gd
+++ modules/interaction/spawner/spawner.gd
@@ -1,17 +1,19 @@
 class_name spawner extends Area2D
 @export var spawnList: SpawnList
 @export var numberOfSpawns: int
+const OFFSET = 100
+var rng : RandomNumberGenerator = RandomNumberGenerator.new() 
 
-func _on_body_entered(body: Node2D) -> void:
-	var collider = $SpawnCollider.get_shape()
-	var rng = RandomNumberGenerator.new()
+func _on_body_entered(_body: Node2D) -> void:
+	await get_tree().create_timer(0.2).timeout
+	var collider: Shape2D = $SpawnCollider.get_shape()
 	while numberOfSpawns > 0:
 		rng.randomize()
 		var index : int = rng.randi_range(0, len(spawnList.list)-1)
-		var entity = spawnList.list[index].instantiate()
-		var x_boundary = collider.size.x * self.transform.x[0]
-		var y_boundary = collider.size.y *self.transform.y[0]
-		entity.position = self.position + Vector2(rng.randf_range(-x_boundary,x_boundary),rng.randf_range(-y_boundary,y_boundary))
-		get_parent().add_child(entity)
+		var entity : Node2D = spawnList.list[index].instantiate()
+		var x_boundary : float = collider.size.x/2 
+		var y_boundary : float= collider.size.y/2 
+		entity.position = self.position + Vector2(rng.randf_range(-x_boundary + OFFSET,x_boundary - OFFSET),rng.randf_range(-y_boundary + OFFSET,y_boundary - OFFSET))
+		get_tree().current_scene.call_deferred("add_child", entity)
 		numberOfSpawns -= 1			
 	queue_free()
