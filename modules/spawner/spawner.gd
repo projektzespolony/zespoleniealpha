eaf3c9c755534a7f40e2d867da051729845e7293
MatKarnicki <matkarnicki@gmail.com>
2025-01-27 20:42:47 +0100

    pre-commit fixes

--- modules/spawner/spawner.gd
+++ modules/spawner/spawner.gd
@@ -12,13 +12,27 @@ func _ready() -> void:
 	spawn_all_entities(0.5, 1.5, enemy_limit, enemy_spawn_locations, enemy_list, false, false)
 	spawn_all_entities(3, 5, item_limit, item_spawn_locations, item_list, true, true)
 
-func spawn_all_entities(lower_time_bound : float, upper_timer_bound : float, limit: int, spawn_location: TileMapLayer, spawn_list : SpawnList, pop_from_list: bool, remove_spawn_tile : bool) -> void:
+
+func spawn_all_entities(
+	lower_time_bound: float,
+	upper_timer_bound: float,
+	limit: int,
+	spawn_location: TileMapLayer,
+	spawn_list: SpawnList,
+	pop_from_list: bool,
+	remove_spawn_tile: bool
+) -> void:
 	for i in range(limit):
 		await get_tree().create_timer(rng.randf_range(lower_time_bound, upper_timer_bound)).timeout
 		spawn_entity(spawn_location, spawn_list, pop_from_list, remove_spawn_tile)
-	
 
-func spawn_entity(spawn_location: TileMapLayer, spawn_list : SpawnList, pop_from_list: bool, remove_spawn_tile : bool) -> void:
+
+func spawn_entity(
+	spawn_location: TileMapLayer,
+	spawn_list: SpawnList,
+	pop_from_list: bool,
+	remove_spawn_tile: bool
+) -> void:
 	var possible_tiles: Array[Vector2i] = spawn_location.get_used_cells()
 	var tile_index: int = rng.randi_range(0, len(possible_tiles) - 1)
 	var entity_index: int = rng.randi_range(0, len(spawn_list.list) - 1)
@@ -29,5 +43,3 @@ func spawn_entity(spawn_location: TileMapLayer, spawn_list : SpawnList, pop_from
 	if remove_spawn_tile:
 		item_spawn_locations.erase_cell(possible_tiles[tile_index])
 	get_tree().current_scene.call_deferred("add_child", entity)
-	
-	
